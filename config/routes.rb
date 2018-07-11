2951bad650cba5e05df07ebbb76ebe2873896f44
Rachel Miller <raeanne1991gmail.com@Rachels-MacBook-Pro.local>
2018-07-11 19:29:32 -0400

    set up controllers and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "home#index"
+  resources :projects, only: [:index, :new, :create, :show, :edit, :destroy]
+  resources :admins, only: [:index, :new, :create, :show, :edit, :destroy]
 end
